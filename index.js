b1acc502172ce269003f6724c6726d62430ab6c3
Szymon <mahiruanina@gmail.com>
2019-02-15 14:17:36 +0100

    module default export

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
+'use strict';
+
 var chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789&.,[](){}?!\'"<>_"\\;`‿⁅∴:';
 var flipped = 'ɐqɔpǝɟɓɥᴉſʞๅɯuodbɹsʇnʌʍxʎzⱯꓭꓛꓷƎꓞꓨHIſꓘꓶWNOꓒῸꓤSꓕꓵꓥMX⅄Z0ƖՇƐh૬9L86⅋˙\'][)(}{¿¡,„><‾„/؛,⁀⁆∵:';
 
 var allChars = chars + flipped;
 var allFlippedChars = flipped + chars;
 
-exports.upsideDown = function (text) {
+var upsideDown = function (text) {
     return text.split('').map(function (letter) {
         return allFlippedChars[allChars.indexOf(letter)];
     }).reverse().join('');
 };
-console.log(exports.upsideDown('uʍopǝpᴉsdn\\ɯʎzsɯnzs\\ɯoɔ˙qnɥʇᴉɓ\\\\:sdʇʇɥ'))
+
+exports['default'] = upsideDown;
+module.exports = exports['default'];
